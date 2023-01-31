d1a1263260304683c04787a0e51364059dc4649a
CesarQuint <pedrocesarquintero001@gmail.com>
2023-01-31 13:39:14 -0600

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,16 @@
+
+
+function Formulario() {
+  return (
+    <>
+
+        <form action="">
+            <div className="block mb-3 font-bold text-gray-400 uppercase">
+
+            </div>
+        </form>
+    </>
+  )
+}
+
+export default Formulario
